534cc7bfd028233d09af92e658eb6c4fc1eafc81
Billy Ceskavich <bceskavich@gmail.com>
2019-02-14 13:01:16 -0500

    Prep Rolodex package for publish (#13)

--- mix.exs
+++ mix.exs
@@ -4,16 +4,19 @@ defmodule Rolodex.MixProject do
   def project do
     [
       app: :rolodex,
+      name: "Rolodex",
+      description: "Automated docs generation",
       version: "0.1.0",
       elixir: "~> 1.7",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      package: package()
     ]
   end
 
   # Run "mix help compile.app" to learn about applications.
-  def application do
+  def application() do
     [
       extra_applications: [:logger]
     ]
@@ -24,7 +27,7 @@ defmodule Rolodex.MixProject do
   defp elixirc_paths(_), do: ["lib"]
 
   # Run "mix help deps" to learn about dependencies.
-  defp deps do
+  defp deps() do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
@@ -32,6 +35,13 @@ defmodule Rolodex.MixProject do
       {:flow, "~> 0.14.3"},
       {:phoenix, "~> 1.4.0"},
       {:dialyxir, "~> 1.0.0-rc.4", only: [:dev], runtime: false}
+    ]
+  end
+
+  defp package() do
+    [
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/Frameio/rolodex"}
     ]
   end
 end
